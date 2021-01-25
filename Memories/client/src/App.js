bd1ec68af51d7384fd78e94d7f1fd61ac43e785d
Kumar Nikhil <knikhil352000@gmail.com>
2021-01-25 09:40:58 +0530

    Adding Styles

--- Memories/client/src/App.js
+++ Memories/client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import memories from './images/memories.png'
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
+import Posts from './Posts/Posts'
+import Form from './Form/Form'
+import useStyles from './styles'
 const App = () => {
+    const classes = useStyles();
     return (
         <Container maxidth='lg'>
-            <AppBar position='static' color='inherit'>
-                <Typography variant='h2' align='center'>Memories</Typography>
+            <AppBar className={classes.appBar} position='static' color='inherit'>
+                <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
                 <img src={memories} alt='memories' height='60'/>
             </AppBar>
             <Grow in>
