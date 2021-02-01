1cd2e4ec271c3fd3d51d20a0125f6ae367725a08
Kumar Nikhil <knikhil352000@gmail.com>
2021-02-01 15:18:58 +0530

    Adding Like Feature

--- Memories/client/src/Posts/Post/Post.js
+++ Memories/client/src/Posts/Post/Post.js
@@ -6,14 +6,14 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment'
 import {useDispatch} from 'react-redux'
-import { deletePost } from '../../actions/post'
+import { deletePost, likePost } from '../../actions/post'
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
     const dispatch = useDispatch(); 
 
     return (
         <Card className={classes.card}>
-            <CardMedia className={classes.media} image={postMessage.selectedFile} title={post.title} />
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
             <div className={classes.overlay}>
                 <Typography variant='h6'>{post.creator}</Typography>
                 <Typography variant='body2'>{moment(post.createdAt).fromNow()}</Typography>
@@ -29,14 +29,14 @@ const Post = ({ post, setCurrentId }) => {
             </div>
             <Typography className={classes.title} variant='h5' gutterBottom>{post.title}</Typography>
             <CardContent>
-                <Typography variant='h5' gutterBottom>{post.message}</Typography>
+                <Typography variant='body2' color='textSecondary' component="p" gutterBottom>{post.message}</Typography>
             
             </CardContent>
             <CardActions className={classes.cardActions}> 
-                <Button size='small' color='primary' onClick={()=> {}}>
+                <Button size='small' color='primary' onClick={()=> dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize='small' />
-                    Like
-                    {post.likeCount}
+                    Like {post.likeCount}
+                      
                 </Button>
                 <Button size='small' color='primary' onClick={()=> dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize='small' />
--- Memories/client/src/reducers/posts.js
+++ Memories/client/src/reducers/posts.js
@@ -1,6 +1,8 @@
 
 const reducer = (posts = [], action) => {
     switch(action.type) {
+        case 'LIKE':
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
         case 'DELETE':
             return posts.filter((post) => post._id !== action.payload)
         case 'FETCH_ALL':
