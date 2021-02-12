dfb96b0f38456156fb782a1480027c0a18558d8d
Kumar Nikhil <knikhil352000@gmail.com>
2021-02-13 00:36:19 +0530

    Creating the auth middleware for liking the post

--- Admin-Memories/server/controllers/user.js
+++ Admin-Memories/server/controllers/user.js
@@ -13,13 +13,34 @@ export const signin = async(req, res) => {
 
         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password)
         if(!isPasswordCorrect) {
-            return res.status(400).json({ message: 'Invalid'});
+            return res.status(400).json({ message: 'Invalid Credential'});
         }
+        const token = jwt.sign({email: existingUser.email, id: existingUser._id}, 'test', { expiresIn: "1h"}) 
+        res.status(200).json({result: existingUser, token})
     
     } catch(error) {
-
+        res.status(500).json({message: 'Something went wrong'})
     }
 }
-export const signin = async(req, res) => {
+export const signup = async(req, res) => {
+    const {email, password, firstName, lastName, confirmPassword} = req.body;
+    try {
+        const existingUser = await User.findOne({email});
+        if(existingUser) {
+            return res.status(404).json({ message: "User already exist."})
+        }
+        if(password !== confirmPassword) {
+            return res.status(400).json({message: 'Passwords dont match.'})
+        }
 
+        const hashedPassword = await bcrypt.hash(password, 12);
+        
+        const result = await User.create({email, password: hashedPassword, name: `${firstName} ${lastName}`})
+        
+        const token = jwt.sign({email: existingUser.email, id: existingUser._id}, 'test', { expiresIn: "1h"});
+        
+        res.status(200).json({result: existingUser, token})
+    } catch (error) {
+        res.status(500).json({message: 'Something went wrong'})
+    }
 }
