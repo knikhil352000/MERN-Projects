eed21c41078a742801c72a5a6ac42339e0fdb63d
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-01 14:06:05 +0530

    Adding SwipeButtons and  styles

--- Tinder-Clone/client/src/App.js
+++ Tinder-Clone/client/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import Header from './Header'
+import SwipeButtons from './SwipeButtons'
 import TinderCards from './TinderCards'
 
 const App = () => {
     return (
         <div className="app">
-
-            {/* Headers */}
             <Header /> 
-            {/* TinderCards */}
             <TinderCards />
-            {/* SwipeButtons */}
+            <SwipeButtons />
         </div>
     )
     
