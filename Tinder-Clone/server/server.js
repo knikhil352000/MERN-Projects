a6693ce0af6948d0651f280e511b023ba2389e35
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-01 20:58:28 +0530

    Setting up axios

--- Tinder-Clone/server/server.js
+++ Tinder-Clone/server/server.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import Cards from './dbCards.js';
-
+import cors from 'cors'
 //App Config
 const app = express();
 const port = 8001;
 
 
 // Middlewares
+app.use(express.json())
+app.use(cors());
 
 // DB Config 
 mongoose.connect('mongodb://localhost:27017/tinderDB',{ useNewUrlParser: true , useUnifiedTopology: true });
@@ -20,13 +22,23 @@ app.get('/', (req, res) => {
     res.status(200).send('Hello World');
 })
 
-app.post('/tinder/card', (req, res) => {
+app.post('/tinder/cards', (req, res) => {
     const dbCard = req.body;
-    Cards.create(dbCards, (err, data) => {
+    Cards.create(dbCard, (err, data) => {
         if(err) {
             res.status(500).send(err);
         } else {
-            res.status(201)
+            res.status(201).send(data)
+        }
+    })
+})
+
+app.get('/tinder/cards', (req, res) => {
+    Cards.find((err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
         }
     })
 })
