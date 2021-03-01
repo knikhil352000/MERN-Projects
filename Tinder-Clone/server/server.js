646509c0913a9122e0f702050339fe2d57a077d2
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-01 14:25:45 +0530

    Setting up the backend server

---
+++ Tinder-Clone/server/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+//App Config
+const app = express();
+const port = 8001;
+
+// Middlewares
+
+// DB Config 
+
+// API Endpoints
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World');
+})
+
+// Listener 
+app.listen(port, () => {
+    console.log('listening at localhost 8001');
+})
