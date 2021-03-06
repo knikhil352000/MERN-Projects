9b0a139c93843bb62e752f39df5b45b283827d50
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-07 02:01:09 +0530

    Setting Up the video player

--- Tiktok-Clone/client/src/App.js
+++ Tiktok-Clone/client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import Header from './Header'
-import SwipeButtons from './SwipeButtons'
-import TinderCards from './TinderCards'
+import Video from './Video'
+
 
 const App = () => {
     return (
         <div className="app">
+            <h1>Hello World</h1>
+            <Video />
+            <Video />
+            {/* <Video />
+            <Video />
+            <Video />
+            <Video />
+            <Video /> */}
         </div>
     )
     
---
+++ Tiktok-Clone/client/src/Video.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Video = () => {
+    return (
+        <div className='video'>
+            {/* <video className='video__player' loop src="https://vod-progressive.akamaized.net/exp=1615076693~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4127%2F17%2F445636626%2F1954201251.mp4~hmac=2ac59c377e1b63b58a85ce046cc2099be1065d1b4e72449faf5b5cf913895a3c/vimeo-prod-skyfire-std-us/01/4127/17/445636626/1954201251.mp4?filename=production+ID%3A5056339.mp4"></video> */}
+            <video src="https://vod-progressive.akamaized.net/exp=1615076752~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3968%2F18%2F469843671%2F2089634529.mp4~hmac=776019479c460919790511c053bed9f21899f24f5252529400694d9a2061c830/vimeo-prod-skyfire-std-us/01/3968/18/469843671/2089634529.mp4?filename=pexels-maksim-goncharenok-5642532.mp4"></video>
+            
+            {/* VideoFooter */}
+            {/* VideoSidebar */}
+        </div>
+    )
+}
+
+export default Video
