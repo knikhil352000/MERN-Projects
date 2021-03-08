7ee80a3f1a0d78aa0e0c91fb429d945d2d4f44d0
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-08 19:04:52 +0530

    Setting up mongoDB

--- Tiktok-Clone/client/src/App.js
+++ Tiktok-Clone/client/src/App.js
@@ -7,8 +7,8 @@ const App = () => {
         <div className="app">
 
             <div className="app__videos">
-                <Video url='https://vod-progressive.akamaized.net/exp=1615076752~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3968%2F18%2F469843671%2F2089634529.mp4~hmac=776019479c460919790511c053bed9f21899f24f5252529400694d9a2061c830/vimeo-prod-skyfire-std-us/01/3968/18/469843671/2089634529.mp4?filename=pexels-maksim-goncharenok-5642532.mp4' />
-                <Video url='https://vod-progressive.akamaized.net/exp=1615076693~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4127%2F17%2F445636626%2F1954201251.mp4~hmac=2ac59c377e1b63b58a85ce046cc2099be1065d1b4e72449faf5b5cf913895a3c/vimeo-prod-skyfire-std-us/01/4127/17/445636626/1954201251.mp4?filename=production+ID%3A5056339.mp4' />
+                <Video url='https://vod-progressive.akamaized.net/exp=1615224095~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3773%2F18%2F468865221%2F2084223033.mp4~hmac=ff8b18838231b30942070404ee6b6a8c31535adc585f42bb8fe1aea20062adcb/vimeo-prod-skyfire-std-us/01/3773/18/468865221/2084223033.mp4?filename=pexels-ekaterina-bolovtsova-5615170.mp4' />
+                <Video url='https://vod-progressive.akamaized.net/exp=1615224127~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F972%2F19%2F479862391%2F2144822668.mp4~hmac=1e146ab73eb72488708746895a0e8feab2ce3213eb7ddc7b9f97f27f30f4647d/vimeo-prod-skyfire-std-us/01/972/19/479862391/2144822668.mp4?filename=pexels-gabby-k-5899825.mp4' />
             </div>
 
         </div>
--- Tiktok-Clone/server/server.js
+++ Tiktok-Clone/server/server.js
@@ -1,9 +1,22 @@
 import express from 'express'
 import mongoose from 'mongoose'
-
+import data from './data.js'
 const app = express();
 const port = 9000;
 
+mongoose.connect('mongodb://localhost:27017/tiktokDB',{ useNewUrlParser: true , useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+    console.log('Connected to the Database');
+})
+
 app.get('/', (req, res) => {
     res.status(200).send('Hello World');
+})
+
+app.get('/v1/posts', (req, res) => {
+    res.status(200).send(data);
+})
+
+app.listen(port, () => {
+    console.log('listening on localhost 9000');
 })
