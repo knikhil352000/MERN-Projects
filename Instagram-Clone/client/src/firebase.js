a0346c0b49218a2830db65a7631d7df51b7927f3
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-10 00:24:49 +0530

    Setting up firebase

---
+++ Instagram-Clone/client/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+import { ApiKey, AuthDomain, ProjectId, StorageBucket, MessagingSenderId, AppId} from './env.js'
+const firebaseApp = {
+    apiKey: ApiKey,
+    authDomain: AuthDomain,
+    projectId: ProjectId,
+    storageBucket: StorageBucket,
+    messagingSenderId: MessagingSenderId,
+    appId: AppId
+};
+
+  export const db = firebaseApp.firestore();
+  export const auth = firebase.auth();
+  export const storage = firebase.storage();
