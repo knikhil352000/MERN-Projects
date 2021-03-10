53441052fb48a3e85e2b263cf08396b9b8bffa7f
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-10 11:31:45 +0530

    Adding sign up modal

--- Instagram-Clone/client/src/App.js
+++ Instagram-Clone/client/src/App.js
@@ -1,26 +1,78 @@
-import React,{ useState } from 'react'
+import React,{ useState, useEffect } from 'react'
 import './App.css'
 import Post from './Post'
+import {db} from './firebase'
+import { Button, Avatar, makeStyles, Modal, Input } from "@material-ui/core";
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+  
+    return {
+      height: "300px",
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: "absolute",
+      width: 400,
+      height: 200,
+      backgroundColor: theme.palette.background.paper,
+      border: "2px solid #000",
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
 const App = () => {
-    const [posts, setPosts] = useState([
-        {
-            username: 'FreecodeCamp', 
-            caption: 'Learning ReactJS', 
-            imageUrl: "https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
-        },
-        {
-            username: 'Nikhil Singh', 
-            caption: 'Feeling Fresh', 
-            imageUrl: "https://avatars.githubusercontent.com/u/71940958?s=460&u=24ef1c10f91e5ef2dffac627465b825a9fef769a&v=4"
-        },
-        {
-            username: 'Anonymous', 
-            caption: 'Eye for A eye', 
-            imageUrl: "https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png"
-        },
-    ]);
+    const classes = useStyles();
+    const [posts, setPosts] = useState([]);
+    const [open, setOpen] = useState(true);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const [modalStyles] = useState(getModalStyle);
+    const handleLogin = () => {
+        
+    }
+    useEffect(() => {
+        db.collection('posts').onSnapshot(snapshot => {
+            setPosts(snapshot.docs.map(doc => ({
+                id: doc.id,
+                post: doc.data(),
+            })))
+        });
+    }, [posts])  
     return (
         <div className="app">
+            <Modal
+                open={open}
+                onClose={() => setOpen(false)}
+            >
+                <div style={modalStyles} className={classes.paper}>
+                    <center>
+                        <img
+                            className="app__headerImage"
+                            src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                            alt=""
+                        />
+                        <Input 
+                            placeholder='email'
+                            type='text'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <Input 
+                            placeholder='password'
+                            type='password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <Button onClick={handleLogin}>Login</Button>
+                    </center>
+                </div>
+            </Modal>
             <div className="app__header">
                 <img
                     className="app__headerImage"
@@ -28,9 +80,10 @@ const App = () => {
                     alt=""
                 />
             </div>
+            <Button onClick={() => setOpen(true)}>Sign Up</Button>
             {
-                posts.map(post => (
-                    <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+                posts.map(({id, post}) => (
+                    <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
                 ))
             }
         
--- Instagram-Clone/client/src/firebase.js
+++ Instagram-Clone/client/src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 import { ApiKey, AuthDomain, ProjectId, StorageBucket, MessagingSenderId, AppId} from './env.js'
-const firebaseApp = {
+const firebaseApp = firebase.initializeApp({
     apiKey: ApiKey,
     authDomain: AuthDomain,
     projectId: ProjectId,
     storageBucket: StorageBucket,
     messagingSenderId: MessagingSenderId,
-    appId: AppId
-};
+    appId: AppId,
+});
 
   export const db = firebaseApp.firestore();
   export const auth = firebase.auth();
