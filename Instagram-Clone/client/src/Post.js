84ae62f97617bcb305b571f1c7818b13870df2f5
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-11 23:58:33 +0530

    setting up comment box

--- Instagram-Clone/client/src/Post.js
+++ Instagram-Clone/client/src/Post.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
 import Avatar from '@material-ui/core/Avatar'
 import './Post.css'
-const Post = ({imageUrl, caption, username}) => {
+import { db } from './firebase'
+const Post = ({imageUrl, caption, username, postId}) => {
+    const [comments, setComments] = useState([])
+    useEffect(() => {
+        if(postId) {
+            let unsubscribe = db
+                .collection('posts')
+                .doc(postId)
+                .collection('comments')
+                .onSnapshot((snapshot) => {
+                    setComments(snapshot.docs.map(doc => doc.data()));
+                })
+        }
+        return () => {
+            unsubscribe();
+        }
+    },[postId])
     return (
         <div className='post'>
             <div className="post__header">
