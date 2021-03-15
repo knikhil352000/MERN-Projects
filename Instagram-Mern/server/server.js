61a54fcca4adcb151a7de2cd1263f67abe010617
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-15 21:33:41 +0530

    Schema and api endpoints

--- Instagram-Mern/server/server.js
+++ Instagram-Mern/server/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
-
+import dbModel from './dbModel.js'
 const app = express();
 const port = 9000;
 
@@ -16,6 +16,28 @@ mongoose.connection.once('open', () => {
 app.get('/', (req, res) => {
     res.status(200).send('Hello World');
 })
+
+app.post('/upload', (req, res) => {
+    const data = req.body;
+    dbModel.create(data, (err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    })
+})
+
+app.get('/sync', (req, res) => {
+    dbModel.find((err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    })
+})
+
 
 app.listen(port, () => {
     console.log('listening at port 9000');
