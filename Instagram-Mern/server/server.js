dd6861cfb52a66f64d55973fc1e7e646e131e771
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-15 21:06:08 +0530

    Setting up the database

--- Instagram-Mern/server/server.js
+++ Instagram-Mern/server/server.js
@@ -6,6 +6,12 @@ const app = express();
 const port = 9000;
 
 app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/instaDB',{ useNewUrlParser: true , useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+    console.log('Connected to the Database');
+})
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello World');
